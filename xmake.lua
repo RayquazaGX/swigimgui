203836f3e82650bd75e616b08d30a9e76b362e7c
域外創音 <kaitensekai@qq.com>
2021-10-13 00:35:33 +0800

    Xmake: add more options

--- xmake.lua
+++ xmake.lua
@@ -1,30 +1,33 @@
 add_rules("mode.release", "mode.debug")
 set_languages("cxx11")
 
-option("backend")
-    set_showmenu(true)
-    set_category("target_backend")
-    set_default("none")
-    set_values("none", "glfw_opengl3")
-option_end()
-
 option("language")
     set_showmenu(true)
-    set_category("target_binding_language")
+    set_category("swig_options/target_binding_language")
     set_default("lua")
     set_values("lua")
 option_end()
 
 option("lua_flavor")
     set_showmenu(true)
-    set_category("lua_options/lua_flavor")
+    set_category("swig_options/lua_options/lua_flavor")
     set_default("luajit")
     set_values("luajit", "lua5.1", "lua5.2", "lua5.3", "lua5.4")
 option_end()
 
-if is_config("backend", "glfw.*") then
-    add_requires("glfw", {configs={glfw_include="es3"}})
-end
+option("backend")
+    set_showmenu(true)
+    set_category("imgui_options/target_backend")
+    set_default("none")
+    set_values("none", "glfw_opengl3")
+option_end()
+
+option("glfw_include")
+    set_showmenu(true)
+    set_category("imgui_options/glfw_options/glfw_include")
+    set_default("glu")
+    set_values("none", "glu", "glext", "vulkan", "es2", "es3")
+option_end()
 
 if is_config("language", "lua.*") then
     if is_config("lua_flavor", "luajit.*") then
@@ -40,13 +43,23 @@ if is_config("language", "lua.*") then
     end
 end
 
+if is_config("backend", "glfw.*") then
+    local checks = {"glu", "glext", "vulkan", "es2", "es3"}
+    local glfwIncludeStr = "none"
+    for i = 1, #checks do if is_config("glfw_include", checks[i]) then glfwIncludeStr = checks[i] end end
+    add_requires("glfw", {configs={glfw_include=glfwIncludeStr}})
+end
+
 local function getPlatformDefineSymbols()
     return (
         is_plat("windows") and {"-D_WIN32"}
         or is_plat("linux") and {"-D__linux__"}
-        or is_plat("macosx") and {"-D__APPLE__"}
-        or is_plat("android") and {"-D__ANDROID__"}
-        or is_plat("iphoneos") and {"-D__APPLE__", "-DTARGET_OS_IOS"}
+        or is_plat("bsd") and {"-D__FreeBSD__"}  -- Untested
+        or is_plat("macosx") and {"-D__APPLE__"}  -- Untested
+        or is_plat("android") and {"-D__ANDROID__"}  -- Untested
+        or is_plat("iphoneos") and {"-D__APPLE__", "-DTARGET_OS_IOS"}  -- Untested
+        or is_plat("wasm") and {"-D__EMSCRIPTEN__"}  -- Untested
+        or {}
     )
 end
 
